8161c32c38298b7b3c18a6fd0ae187ecd4123d22
Cheolwon Park <152289594+Choroning@users.noreply.github.com>
2025-07-31 20:21:48 +0900

    Update A10104-1_C05Q2.c

--- Chapter05_Control-Statements/A10104-1_C05Q2.c
+++ Chapter05_Control-Statements/A10104-1_C05Q2.c
@@ -1,11 +1,11 @@
 /**
- * @file    A10104-1_C5Q2.c
+ * @file    A10104-1_C05Q2.c
  * @brief   Q2. 섭씨 ↔ 화씨 단위 변환
  * @author  박철원 (Cheolwon Park)
  * @date    2024-04-02
  * 
- * @note	소스 코드에 사용된 scanf_s는 Visual Studio 이외의
- * 			IDE에서 실행이 불가할 수 있음에 주의하시기 바랍니다.
+ * @note    소스 코드에 사용된 scanf_s는 Visual Studio 이외의
+ * 		    IDE에서 실행이 불가할 수 있음에 주의하시기 바랍니다.
  */
 
 #include <stdio.h>
