5a0df92ba250af3f6015648fcf4c198b28be396e
Cheolwon Park <152289594+Choroning@users.noreply.github.com>
2025-07-31 20:37:41 +0900

    Update A10104-1_C09Q1.c

--- Chapter09_Strings/A10104-1_C09Q1.c
+++ Chapter09_Strings/A10104-1_C09Q1.c
@@ -1,11 +1,11 @@
 /**
- * @file    A10104-1_C9Q1.c
+ * @file    A10104-1_C09Q1.c
  * @brief   Q1. 문자열 조작
  * @author  박철원 (Cheolwon Park)
  * @date    2024-05-28
  * 
- * @note	소스 코드에 사용된 gets_s는 Visual Studio 이외의
- * 			IDE에서 실행이 불가할 수 있음에 주의하시기 바랍니다.
+ * @note    소스 코드에 사용된 gets_s는 Visual Studio 이외의
+ *          IDE에서 실행이 불가할 수 있음에 주의하시기 바랍니다.
  */
 
  #include <stdio.h>
